5aa99c8aa2eefaa1b76729d93c1d8b38c2c74f20
weichanglu <cl9000@126.com>
2019-02-18 18:09:04 +0800

    Update WebviewProgressLine.h

--- WYWebViewDemo-进度条/WYWebViewDemo/WYWebViewDemo/Web2/WebviewProgressLine.h
+++ WYWebViewDemo-进度条/WYWebViewDemo/WYWebViewDemo/Web2/WebviewProgressLine.h
@@ -2,7 +2,7 @@
 //  WebviewProgressLine.h
 //  WYWebViewDemo
 //
-//  Created by I2finance on 2018/6/22.
+//  Created by cl9000 on 2018/6/22.
 //  Copyright © 2018年 wangyan. All rights reserved.
 //
 
