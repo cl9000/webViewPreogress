369e8ad3f87efe0926c6045ec46ff4f1015ed0ad
weichanglu <cl9000@126.com>
2019-02-18 18:04:52 +0800

    Update WebViewController.h

--- WYWebViewDemo-进度条/WYWebViewDemo/WYWebViewDemo/Web2/WebViewController.h
+++ WYWebViewDemo-进度条/WYWebViewDemo/WYWebViewDemo/Web2/WebViewController.h
@@ -2,7 +2,7 @@
 //  WebViewController.h
 //  WYWebViewDemo
 //
-//  Created by I2finance on 2018/6/22.
+//  Created by cl9000 on 2018/6/22.
 //  Copyright © 2018年 wangyan. All rights reserved.
 //
 
